a4f403d06fb2d8498795ff4c8c7b8fc07c91d23f
fajarmagsyar <fajar.magsyar@gmail.com>
2022-08-14 23:35:44 +0800

    buat project

---
+++ app/Config/Routes.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Config;
+
+$routes = Services::routes();
+
+if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+
+
+//Taruh route disebelah sini:
+$routes->get('/', 'Home::index');
+$routes->get('/bruh', function () {
+    echo "something";
+});
+
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
